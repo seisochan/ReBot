1b8cd581a00044a06c860434962d330e75b570a5
seiso-chan <yuzukishiramine@gmail.com>
2022-07-30 23:23:22 +0700

    first commit

---
+++ config.example.js
@@ -0,0 +1,14 @@
+module.exports = {
+  prefix: '?',
+  token: '',
+  roles: {
+    admin: ['Moderator', 'Owner'],
+    live: ['Moderator', 'Member+', 'Pawang Bot', 'Owner'],
+  },
+  activity: 'Use ? to start command',
+  textChannelID: {
+    live: '',
+    announce: ''
+  },
+  googleApi: '',
+};
